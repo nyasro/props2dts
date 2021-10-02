e159d0698caa599b1c786b6c654fd720b637c44b
nyasro <nyasro@gmail.com>
2021-10-03 00:02:00 +0200

    make cli

---
+++ cli.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const react2dts = require('react-to-typescript-definitions')
+const glob = require('glob')
+const path = require('path')
+const fs = require('fs')
+
+const [,, src] = process.argv
+
+if(!src) {
+  console.log('Directory path is missing.')
+}
+
+glob(`${src}/**/*.jsx`, {},function (er, files) {
+
+  files.forEach((file) => {
+
+    const parsePath = path.parse(file)
+    const dtsFile = `${parsePath.dir}/${parsePath.name}.d.ts`
+
+    let dtsData = react2dts
+      .generateFromFile('Components', file)
+
+    dtsData = dtsData
+      .trim()
+      .slice(0, -1)
+      .split('\n')
+      .slice(3)
+      .map((line) => {
+        return line.replace(/^\s{4}/, '')
+      })
+      .join('\n')
+      .replace('const ', 'declare const ')
+
+    fs.writeFileSync(dtsFile, dtsData)
+
+  })
+})
